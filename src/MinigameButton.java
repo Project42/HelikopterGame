a0578b85f546a5f2d3233eb1ef2b8831ca2e903a
Radek Slupik <daknok@icloud.com>
2012-11-27 12:14:25 +0100

    Add minigame button class
    The click behavior is not yet implemented.

---
+++ src/MinigameButton.java
@@ -0,0 +1,9 @@
+import greenfoot.*;
+
+public class MinigameButton extends Actor {
+    public void act() {
+        if (Greenfoot.mouseClicked(this)) {
+            throw new UnsupportedOperationException("not implemented");
+        }
+    }
+}
