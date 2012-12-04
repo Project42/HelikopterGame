cf1ae39d86885a4830a187bc81ffb0e31c0c1afb
Radek Slupik <daknok@icloud.com>
2012-12-04 13:54:08 +0100

    Add speed power-up

--- src/Helicopter.java
+++ src/Helicopter.java
@@ -1,14 +1,30 @@
 import greenfoot.*;
 
 public class Helicopter extends Actor {
+    private int speed;
+
+    public Helicopter() {
+        speed = 8;
+    }
+
     public void act() {
-        if (Greenfoot.isKeyDown("w")) move(0, -16);
-        if (Greenfoot.isKeyDown("s")) move(0, +16);
-        if (Greenfoot.isKeyDown("a")) move(-16, 0);
-        if (Greenfoot.isKeyDown("d")) move(+16, 0);
+        if (Greenfoot.isKeyDown("w")) move(0, -speed);
+        if (Greenfoot.isKeyDown("s")) move(0, +speed);
+        if (Greenfoot.isKeyDown("a")) move(-speed, 0);
+        if (Greenfoot.isKeyDown("d")) move(+speed, 0);
+
+        consumePowerUp();
     }
 
     private void move(int dx, int dy) {
         setLocation(getX() + dx, getY() + dy);
+    }
+
+    private void consumePowerUp() {
+        Actor powerUp = getOneIntersectingObject(SpeedPowerUp.class);
+        if (powerUp != null) {
+            powerUp.getWorld().removeObject(powerUp);
+            speed = 16;
+        }
     }
 }
---
+++ src/SpeedPowerUp.java
@@ -0,0 +1,5 @@
+import greenfoot.*;
+
+public class SpeedPowerUp extends Actor {
+
+}
