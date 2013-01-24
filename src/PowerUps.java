71cdfad4ac336d87a3664025bd54569693c167c2
CVD <cornelisvdam@hotmail.com>
2013-01-25 00:32:38 +0100

    cleaned dat shit

--- src/PowerUps.java
+++ src/PowerUps.java
@@ -1,17 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class PowerUps here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class PowerUps extends Actor
 {
-    /**
-     * Act - do whatever the PowerUps wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
         Actor wallcheck = getOneObjectAtOffset(0, 0, Wall.class);
